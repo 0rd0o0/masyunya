8d2098eaf8da2c712933702aaa288a81d699e06e
0rd0o0 <107910621+0rd0o0@users.noreply.github.com>
2022-06-21 13:40:30 +0600

    Adjusting masyunya pic scale

--- masyunyaForms/masyunyaForms/Form1.Designer.cs
+++ masyunyaForms/masyunyaForms/Form1.Designer.cs
@@ -37,18 +37,20 @@
             // 
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(681, 321);
+            this.pictureBox1.Location = new System.Drawing.Point(502, 254);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(133, 128);
+            this.pictureBox1.Size = new System.Drawing.Size(128, 129);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(855, 485);
+            this.ClientSize = new System.Drawing.Size(641, 394);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
